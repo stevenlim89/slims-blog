7a26408ba40e6e29a32975d4cfcbda7b59961bed
Steven Lim <stevenlim890@gmail.com>
2018-08-11 15:11:33 -0700

    configured partials

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    shortUrl = require('./processing/url');
+	handlebars = require("express-handlebars"),
+    app = express();
 
 // set views directory
-app.engine('html', require('ejs').renderFile);
+app.engine('html', handlebars({ 
+	extname: '.html',
+	partialsDir: __dirname + '/public/partials'
+}));
+
 app.set('view engine', 'html');
-app.set('views', __dirname + '/public/views');
+app.set('views', __dirname + '/public');
 app.use(express.static(__dirname + '/public'));
 
 // Body parser for req.body
@@ -16,24 +19,18 @@ var parser = {
 app.use(parser.body.urlencoded({ extended: true }));
 app.use(parser.body.json());
 
-// Post and Get methods
+// Get methods
 app.get('/', (req, res) => {
 	res.render('home');
 });
 
-app.get('/:code', shortUrl.redirect);
-
-app.post('/getShortenedURL', shortUrl.getShortenedURL);
+app.get('/about', (req, res) => {
+	res.render('about');
+});
 
 // Database stuff
 var port = 5000;
-mongoose.connect('mongodb://stallen:hello@ds011258.mlab.com:11258/heroku_q93csxz1');
-var connection = mongoose.connection;
-connection.on('error', console.error.bind(console, 'Unsuccessful connection'));
-connection.once('open', (callback) => {
-	console.log('Connection was successful');
-});
 
-app.listen(port, (req, res) => {
+app.listen(process.env.PORT || port, (req, res) => {
 	console.log(`Listening on port ${port}`);
 });
--- models.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var url = new mongoose.Schema({
-	originalUrl: String,
-	newUrl: String
-});
-
-exports.Url = mongoose.model('Url', url);
